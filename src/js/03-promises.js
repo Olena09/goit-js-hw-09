9cb3f5ce3c23194c8bc65483246fe64962fe26f4
Olenka Vavrychuk <40758661+Olena09@users.noreply.github.com>
2023-10-02 11:35:28 +0300

    start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+  if (delay.value < 0 || step.value < 0 || amount.value < 0) {
+    alert(`Incorrect!`);
   } else {
-    // Reject
+    for (let i = 0; i < amount.value; i += 1) {
+      let position = i + 1;
+      const newDelay = Number(delay.value) + step.value * i;
+      createPromise(position, newDelay)
+        .then(({ position, delay }) => {
+          alert(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          alert(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
   }
+  event.currentTarget.reset();
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+
+
